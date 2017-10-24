f5ec1caea026ee2d469743ac18f30ba7e305f142
Herb Stahl <ghstahl@gmail.com>
2017-10-24 14:54:22 -0700

    Bringing in external SPA

--- src/ReferenceWebApp.ExternalIdentity/Controllers/HomeController.cs
+++ src/ReferenceWebApp.ExternalIdentity/Controllers/HomeController.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using P7.SessionContextStore.Core;
 using ReferenceWebApp.Models;
 
 namespace ReferenceWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ISessionContextStore _sessionContextStore;
+        public HomeController(ISessionContextStore sessionContextStore)
+        {
+            _sessionContextStore = sessionContextStore;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -35,10 +42,11 @@ namespace ReferenceWebApp.Controllers
             return View(result);
         }
 
-        public IActionResult Contact()
+        public async Task<IActionResult> Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
+            ISessionContext sessionContext = await _sessionContextStore.GetSessionContextAsync();
+            ViewData["some-data"] = await sessionContext.GetValueAsync<SomeData>("some-data");
             return View();
         }
 
