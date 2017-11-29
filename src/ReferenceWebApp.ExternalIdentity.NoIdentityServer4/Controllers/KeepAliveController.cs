62261cbc2ae9d4da248793c901b59d280d1ce05a
Herb Stahl <ghstahl@gmail.com>
2017-11-29 13:49:54 -0800

    added keep-alive-hook.js

---
+++ src/ReferenceWebApp.ExternalIdentity.NoIdentityServer4/Controllers/KeepAliveController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReferenceWebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("keep-alive")]
+    public class KeepAliveController : Controller
+    {
+        // GET: api/KeepAlive
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            return "OK";
+        }
+    }
+}
