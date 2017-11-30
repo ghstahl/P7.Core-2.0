591e6d42616cbd3048de4af6d19b86de6396ce61
Herb Stahl <ghstahl@gmail.com>
2017-11-30 15:04:57 -0800

    transpiled js

--- src/ReferenceWebApp.ExternalIdentity.NoIdentityServer4/wwwroot/lib/pretty-json.js
+++ src/ReferenceWebApp.ExternalIdentity.NoIdentityServer4/wwwroot/lib/pretty-json.js
@@ -1,26 +1,23 @@
-var prettyPrint = (function () {
+'use strict';
+
+var prettyPrint = function () {
     var library = {};
     library.json = {
-        replacer: function (match, pIndent, pKey, pVal, pEnd) {
+        replacer: function replacer(match, pIndent, pKey, pVal, pEnd) {
             var key = '<span class=json-key>';
             var val = '<span class=json-value>';
             var str = '<span class=json-string>';
             var r = pIndent || '';
-            if (pKey)
-                r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
-            if (pVal)
-                r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
+            if (pKey) r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+            if (pVal) r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
             return r + (pEnd || '');
         },
-        prettyPrint: function (obj) {
+        prettyPrint: function prettyPrint(obj) {
             var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
-            return JSON.stringify(obj, null, 3)
-                .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
-                .replace(/</g, '&lt;').replace(/>/g, '&gt;')
-                .replace(jsonLine, library.json.replacer);
+            return JSON.stringify(obj, null, 3).replace(/&/g, '&amp;').replace(/\\"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(jsonLine, library.json.replacer);
         }
     };
 
     return library;
-})();
+}();
  
--- src/ReferenceWebApp.ExternalIdentity/wwwroot/lib/keep-alive-hook.js
+++ src/ReferenceWebApp.ExternalIdentity/wwwroot/lib/keep-alive-hook.js
@@ -1,70 +1,72 @@
-var keepAliveHook = (function () {
+'use strict';
+
+var keepAliveHook = function () {
+    var _this = this,
+        _arguments = arguments;
+
     var library = {};
-    let w = window;
+    var w = window;
     w._keepAlive = false;
 
     w._oldOpen = XMLHttpRequest.prototype.open;
-    let onStateChange = (event) => {
+    var onStateChange = function onStateChange(event) {
         if (event.currentTarget.readyState === 4) {
             library._onHttpMonitor(event.currentTarget.responseURL, event.currentTarget.status);
         }
     };
-    XMLHttpRequest.prototype.open = () => {
+    XMLHttpRequest.prototype.open = function () {
         // when an XHR object is opened, add a listener for its readystatechange events
-        this.addEventListener('readystatechange', onStateChange);
+        _this.addEventListener('readystatechange', onStateChange);
         // run the real `open`
-        w._oldOpen.apply(this, arguments);
+        w._oldOpen.apply(_this, _arguments);
     };
     if (!window.fetch.polyfill) {
         w._oldFetch = w.fetch;
 
-        window.fetch = (input, init) => {
-            return w._oldFetch(input, init).then(response => {
+        window.fetch = function (input, init) {
+            return w._oldFetch(input, init).then(function (response) {
                 library._onHttpMonitor(response.url, response.status);
                 return response;
             });
         };
     }
-    library.start = (keepAliveUrl) => {
+    library.start = function (keepAliveUrl) {
         library._keepAliveUrl = keepAliveUrl;
-        library.timer = setInterval(() => {
+        library.timer = setInterval(function () {
             library._onTimer();
         }, 5000);
-
-    }
-    library.stop = () => {
+    };
+    library.stop = function () {
         library._keepAliveUrl = null;
         if (self.timer) {
-            clearInterval(this.timer);
+            clearInterval(_this.timer);
         }
-    }
-    
-    library._onHttpMonitor = (url, status) =>{
-        let n = url.startsWith(window.location.origin);
+    };
+
+    library._onHttpMonitor = function (url, status) {
+        var n = url.startsWith(window.location.origin);
 
         if (n === false) {
             w._keepAlive = true;
         }
-    }
-    library._onTimer=()=> {
+    };
+    library._onTimer = function () {
         if (w._keepAlive) {
             w._keepAlive = false;
             if (library._keepAliveUrl) {
-                console.log("must keep alive")
-                fetch(library._keepAliveUrl,
-                        {
-                            method: 'GET',
-                            headers: { 'Content-Type': 'text/plain' },
+                console.log("must keep alive");
+                fetch(library._keepAliveUrl, {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'text/plain' }
 
-                        })
-                    .then(res => res.json())
-                    .then(function (res) {
-                        console.log(res);
-                    });
+                }).then(function (res) {
+                    return res.json();
+                }).then(function (res) {
+                    console.log(res);
+                });
             }
         }
-    }
-   
+    };
+
     return library;
-})();
- 
+}();
