d1ce769dd2bfd2df2c84b488ac21c56ad7edc0d7
ghstahl@gmail.com <ghstahl@gmail.com>
2017-12-03 21:50:49 -0800

    Caching the extspa injected data

--- src/P7.External.SPA.Core/ExternalSPARecord.cs
+++ src/P7.External.SPA.Core/ExternalSPARecord.cs
@@ -1,28 +1,37 @@
 using Newtonsoft.Json;
+using ZeroFormatter;
 
 namespace P7.External.SPA.Core
 {
+    [ZeroFormattable]
     public class ExternalSPARecord
     {
         [JsonProperty("renderTemplate")]
-        public string RenderTemplate { get; set; }
+        [Index(0)]
+        public virtual string RenderTemplate { get; set; }
 
         [JsonProperty("key")]
-        public string Key { get; set; }
+        [Index(1)]
+        public virtual string Key { get; set; }
 
         [JsonProperty("requireAuth")]
-        public bool RequireAuth { get; set; }
+        [Index(2)]
+        public virtual bool RequireAuth { get; set; }
 
         [JsonProperty("strongLoginRequiredSeconds")]
-        public int StrongLoginRequiredSeconds { get; set; }
+        [Index(3)]
+        public virtual int StrongLoginRequiredSeconds { get; set; }
 
         [JsonProperty("view")]
-        public string View { get; set; }
+        [Index(4)]
+        public virtual string View { get; set; }
 
         [JsonProperty("clientId")]
-        public string ClientId { get; set; }
+        [Index(5)]
+        public virtual string ClientId { get; set; }
 
         [JsonProperty("redirectUri")]
-        public string RedirectUri { get; set; }
+        [Index(6)]
+        public virtual string RedirectUri { get; set; }
     }
 }
--- src/P7.External.SPA/Areas/ExtSpa/Controllers/HomeController.cs
+++ src/P7.External.SPA/Areas/ExtSpa/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection.Metadata;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -6,24 +8,39 @@ using Microsoft.Extensions.Logging;
 using P7.External.SPA.Core;
 using P7.External.SPA.Models;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using IdentityModel;
 using IdentityModel.Client;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using P7.External.SPA.Filters;
+using ZeroFormatter;
 
 namespace P7.External.SPA.Areas.ExtSpa.Controllers
 {
-    class ViewBagRecord
+ 
+    [ZeroFormattable]
+    public class ViewBagRecord
     {
-        public string AuthorizeUrl { get; set; }
-        public string AuthorizeEndpoint { get; set; }
-        public dynamic SpaRecord { get; set; }
+        // Index is key of serialization
+        [Index(0)]
+        public virtual string AuthorizeEndpoint { get; set; }
+
+        [Index(1)]
+        public virtual string AuthorizeUrl { get; set; }
+
+        [Index(2)]
+        public virtual ExternalSPARecord SpaRecord { get; set; }
     }
+
+
+    
     [Area("ExtSPA")]
     public class HomeController : Controller
     {
+        private readonly IDistributedCache _cache;
         private ILogger Logger { get; set; }
         private readonly IHttpContextAccessor _httpContextAccessor;
         private ISession Session => _httpContextAccessor.HttpContext.Session;
@@ -34,12 +51,14 @@ namespace P7.External.SPA.Areas.ExtSpa.Controllers
             IExternalSPAStore externalSpaStore,
             IConfiguration configuration,
             DiscoveryCache discoveryCache,
+            IDistributedCache cache,
             ILogger<HomeController> logger)
         {
             _httpContextAccessor = httpContextAccessor;
             _configuration = configuration;
             _externalSpaStore = externalSpaStore;
             _discoveryCache = discoveryCache;
+            _cache = cache;
             Logger = logger;
         }
         
@@ -49,21 +68,34 @@ namespace P7.External.SPA.Areas.ExtSpa.Controllers
             var spa = _externalSpaStore.GetRecord(id);
             var result = HttpContext.User.Claims.Select(
                 c => new ClaimType {Type = c.Type, Value = c.Value});
-           
 
-            var clientId = _configuration["Norton-ClientId"];
-            var cientSecret = _configuration["Norton-ClientSecret"];
-            var doc = await _discoveryCache.GetAsync();
+            var cacheKey = $".extSpaViewBagRecord.{id}";
 
-            var request = new AuthorizeRequest(doc.AuthorizeEndpoint);
-            var url = request.CreateAuthorizeUrl(
-                clientId: spa.ClientId,
-                responseType: OidcConstants.ResponseTypes.Code,
-                prompt: OidcConstants.PromptModes.None,
-                redirectUri: spa.RedirectUri,
-                scope: "openid profile email");
+            ViewBagRecord viewBagRecord = null;
+            var value = await _cache.GetAsync(cacheKey);
+            if (value != null)
+            {
+                viewBagRecord = ZeroFormatterSerializer.Deserialize<ViewBagRecord>(value);
+            }
+            else
+            {
+                var doc = await _discoveryCache.GetAsync();
 
-            var viewBagRecord = new ViewBagRecord {AuthorizeEndpoint  = doc.AuthorizeEndpoint,AuthorizeUrl = url, SpaRecord = spa};
+                var request = new AuthorizeRequest(doc.AuthorizeEndpoint);
+                var url = request.CreateAuthorizeUrl(
+                    clientId: spa.ClientId,
+                    responseType: OidcConstants.ResponseTypes.Code,
+                    prompt: OidcConstants.PromptModes.None,
+                    redirectUri: spa.RedirectUri,
+                    scope: "openid profile email");
+
+                viewBagRecord = new ViewBagRecord { AuthorizeEndpoint = doc.AuthorizeEndpoint, AuthorizeUrl = url, SpaRecord = spa };
+                var val = ZeroFormatterSerializer.Serialize(viewBagRecord);
+                var cacheEntryOptions = new DistributedCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromHours(24));
+                _cache.Set(cacheKey, val, cacheEntryOptions);
+            }
+ 
             ViewBag.ViewBagRecord = viewBagRecord;
 
             //	var url = "https://login-int.norton.com/sso/idp/OIDC?prompt=none&response_type=code&scope=openid%20profile%20email&client_id=test_lifelock/p7core/io&redirect_uri=https://p7core.127.0.0.1.xip.io:44311/lifelock/signin-norton";
