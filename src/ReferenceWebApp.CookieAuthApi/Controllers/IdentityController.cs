d42d8c8a5724353f5c7f7d5cda8ec7c1131e56ea
Herb Stahl <ghstahl@gmail.com>
2017-12-09 09:27:09 -0800

    initial

---
+++ src/ReferenceWebApp.CookieAuthApi/Controllers/IdentityController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReferenceWebApp.CookieAuthApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Identity")]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("establish-session")]
+        public async Task EstablishAuthenticatedSession(string username, string password)
+        {
+
+            var properties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+                ExpiresUtc = new DateTime(2017, 12, 31)
+            };
+
+            var claims = new[] {new Claim("name", username), new Claim(ClaimTypes.Role, "User")};
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            await
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity),
+                    properties);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("sign-out")]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("check-authentication")]
+        public async Task<bool> CheckAuthentication()
+        {
+            return User.Identity.IsAuthenticated;
+        }
+    }
+}
---
+++ src/ReferenceWebApp.CookieAuthApi/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ReferenceWebApp.CookieAuthApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    options =>
+                    {
+                        options.LoginPath = "/Account/LogIn"; ;
+                        options.AccessDeniedPath = new PathString("/account/login");
+                        options.Cookie.Name = "AUTHCOOKIE";
+                        options.ExpireTimeSpan = new TimeSpan(1, 0, 0, 0);
+                        options.Cookie.HttpOnly = true;
+                    }
+                );
+
+            services.AddMemoryCache();
+            services.AddSession();
+            services.AddMvc();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            var cookiePolicyOptions = new CookiePolicyOptions
+            {
+                Secure = CookieSecurePolicy.SameAsRequest
+            };
+
+            app.UseCookiePolicy(cookiePolicyOptions);
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
