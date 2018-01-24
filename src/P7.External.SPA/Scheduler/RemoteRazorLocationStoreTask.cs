00a0c28fce3bf971827cf209d5506133651ea43b
ghstahl@gmail.com <ghstahl@gmail.com>
2018-01-24 08:45:44 -0800

    file sync seems to work, at least locally.  Now to see if it works for the webapp

--- src/P7.External.SPA/Scheduler/RemoteRazorLocationStoreTask.cs
+++ src/P7.External.SPA/Scheduler/RemoteRazorLocationStoreTask.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -13,6 +14,7 @@ using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
 using P7.Core.Scheduler.Scheduling;
 using P7.Core.Utils;
+using P7.HealthCheck.Core;
 using P7.RazorProvider.Store.Core;
 using P7.RazorProvider.Store.Core.Interfaces;
 using P7.RazorProvider.Store.Core.Models;
@@ -20,11 +22,21 @@ using Serilog;
 
 namespace P7.External.SPA.Scheduler
 {
+    public partial class MetaData
+    {
+        [JsonProperty("timeStamp")]
+        public string TimeStamp { get; set; }
+    }
+
     public partial class FilesManifest
     {
+        [JsonProperty("metaData")]
+        public MetaData MetaData { get; set; }
+
         [JsonProperty("files")]
         public string[] Files { get; set; }
     }
+
     public partial class FilesManifest
     {
         public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
@@ -37,10 +49,15 @@ namespace P7.External.SPA.Scheduler
 
     public class RemoteFileSyncTask : IScheduledTask
     {
+        private IHealthCheckStore HealthCheckStore { get; set; }
+        private static FilesManifest FilesManifest { get; set; }
         static Serilog.ILogger logger = Log.ForContext<RemoteFileSyncTask>();
+        private IHostingEnvironment _env { get; set; }
         private IConfiguration _config { get; set; }
-        public RemoteFileSyncTask(IConfiguration config)
+        public RemoteFileSyncTask(IConfiguration config,IHostingEnvironment env, IHealthCheckStore healthCheckStore)
         {
+            HealthCheckStore = healthCheckStore;
+            _env = env;
             _config = config;
         }
         public string Schedule => "*/1 * * * *";  // every 1 minute
@@ -64,11 +81,67 @@ namespace P7.External.SPA.Scheduler
             }
             return null;
         }
+        public static async Task<byte[]> GetRemoteContentAsync(string url)
+        {
+            var byteResult = await RemoteFetch.FetchAsync(url, new WebRequestInit() {  });
+            return byteResult;
+        }
         public async Task Invoke(CancellationToken cancellationToken)
         {
+            var key = "remote-file-sync";
+            var currentHealth = await HealthCheckStore.GetHealthAsync(key);
+            if (currentHealth == null)
+            {
+                currentHealth = new HealthRecord()
+                {
+                    Healty = false,
+                    State = null
+                };
+                await HealthCheckStore.SetHealthAsync(key, currentHealth);
+            }
+
             var filesConfig = await GetRemoteFilesConfigAsync(
                 "https://rawgit.com/ghstahl/P7.Core-2.0.RemoteData/master/spas/AngularServerSide/files.json");
+            bool newFile = true;
+            if (FilesManifest != null)
+            {
+                newFile = FilesManifest.MetaData.TimeStamp != filesConfig.MetaData.TimeStamp;
+            }
 
+            if (newFile)
+            {
+                FilesManifest = filesConfig;
+
+                var outDir = Path.Combine(_env.ContentRootPath, "out/");
+                Directory.CreateDirectory(outDir);
+                foreach (var file in filesConfig.Files)
+                {
+                    var fileUrl = $"https://rawgit.com/ghstahl/P7.Core-2.0.RemoteData/master/spas/AngularServerSide/{file}";
+                    var fileData = await GetRemoteContentAsync(fileUrl);
+                    var finalPath = Path.Combine(outDir, file);
+                    var index = finalPath.LastIndexOf('/');
+                    var finalDir = finalPath.Substring(0, index);
+                    Directory.CreateDirectory(finalDir);
+                    try
+                    {
+                        File.WriteAllBytes(finalPath, fileData);
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+            }
+            if (!currentHealth.Healty)
+            {
+                // once loaded we are good
+                currentHealth = new HealthRecord()
+                {
+                    Healty = true,
+                    State = null
+                };
+                await HealthCheckStore.SetHealthAsync(key, currentHealth);
+            }
         }
     }
 
