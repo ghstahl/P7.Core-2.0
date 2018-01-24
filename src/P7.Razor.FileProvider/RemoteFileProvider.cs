020d17397add922c3f67d70a11b925f8ee9921b4
Herb Stahl <ghstahl@gmail.com>
2018-01-23 19:24:16 -0800

    missing

---
+++ src/P7.Razor.FileProvider/RemoteFileProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+namespace P7.Razor.FileProvider
+{
+    public class RemoteFileProvider : IFileProvider, IDisposable
+    {
+        private readonly IDistributedCache _cache = null;
+        public RemoteFileProvider(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            return new NotFoundDirectoryContents();
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
