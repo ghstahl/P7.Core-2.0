9519ed1f614189b1702a710d05068e7fd1080572
ghstahl@gmail.com <ghstahl@gmail.com>
2018-02-13 18:33:38 -0800

    fetch('/api/cors-me/redirect-to?url=https%3A%2F%2Fp7cookieauth.127.0.0.1.xip.io%3A44310%2Fapi%2Fcors-me%2Fredirect-to%3Furl%3Dhttps%3A%2F%2Fp7core.127.0.0.1.xip.io%3A44311%2Fapi%2Fcors-me%2Fecho%3Fmessage%3Ddog', {             method: 'get',             headers:new Headers({             'Content-Type': 'application/json',             'Accept-Type': 'application/json'}),             credentials: 'include',             mode: 'cors',             redirect: 'follow'         })         .then(res => res.json())         .then(function (response) {             console.log(response);         }).catch(function(err) {             // Error :(             console.log(err);         });
    redirect/cors  store.
    
    1. Call host to redirect to another app
    2. Other app redirects back to host
    3. Finally host gets called

---
+++ src/ReferenceWebApp.CookieAuthApi/Controllers/CorsMeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReferenceWebApp.CookieAuthApi.Controllers
+{
+    class EchoPayload
+    {
+        public string Message { get; set; }
+    }
+
+    [Produces("application/json")]
+    [Route("api/cors-me")]
+    public class CorsMeController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("redirect-to")]
+        public async Task<IActionResult> GetRedirectToAsync(string url)
+        {
+
+            return new RedirectResult(url);
+        }
+
+        [HttpOptions]
+        [AllowAnonymous]
+        [Route("redirect-to")]
+        public async Task OptionsRedirectToAsync(string url)
+        {
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("echo")]
+        public async Task<IActionResult> GetEchoAsync(string message)
+        {
+            return Json(new EchoPayload(){Message = message});
+        }
+
+        [HttpOptions]
+        [AllowAnonymous]
+        [Route("echo")]
+        public async Task OptionsEchoAsync(string message)
+        {
+        }
+    }
+}
---
+++ src/ReferenceWebApp.ExternalIdentity.NoIdentityServer4/Controllers/CorsMeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReferenceWebApp.Controllers
+{
+    class EchoPayload
+    {
+        public string Message { get; set; }
+    }
+
+    [Produces("application/json")]
+    [Route("api/cors-me")]
+    public class CorsMeController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("redirect-to")]
+        public async Task<IActionResult> GetRedirectToAsync(string url)
+        {
+
+            return new RedirectResult(url);
+        }
+
+        [HttpOptions]
+        [AllowAnonymous]
+        [Route("redirect-to")]
+        public async Task OptionsRedirectToAsync(string url)
+        {
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("echo")]
+        public async Task<IActionResult> GetEchoAsync(string message)
+        {
+            return Json(new EchoPayload() { Message = message });
+        }
+
+        [HttpOptions]
+        [AllowAnonymous]
+        [Route("echo")]
+        public async Task OptionsEchoAsync(string message)
+        {
+        }
+    }
+}
