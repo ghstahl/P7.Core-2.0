bf7204aa03dafd371993211cc058a2f9030e011e
ghstahl@gmail.com <ghstahl@gmail.com>
2018-02-16 08:29:46 -0800

    using porthole to send oidc data back to parent

---
+++ src/Reference.OIDCApp/Pages/Account/OIDCIFrameResult.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Reference.OIDCApp.Data;
+
+namespace Reference.OIDCApp.Pages.Account
+{
+    public class OIDCIFrameResultModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<LoginModel> _logger;
+        public string Error { get; set; }
+        public Dictionary<string, string> OIDC { get; set; }
+
+        public OIDCIFrameResultModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+        private async Task<Dictionary<string, string>> HarvestOidcDataAsync()
+        {
+            var at = await HttpContext.GetTokenAsync(IdentityConstants.ExternalScheme, "access_token");
+            var idt = await HttpContext.GetTokenAsync(IdentityConstants.ExternalScheme, "id_token");
+            var rt = await HttpContext.GetTokenAsync(IdentityConstants.ExternalScheme, "refresh_token");
+            var tt = await HttpContext.GetTokenAsync(IdentityConstants.ExternalScheme, "token_type");
+            var ea = await HttpContext.GetTokenAsync(IdentityConstants.ExternalScheme, "expires_at");
+
+            var oidc = new Dictionary<string, string>
+            {
+                {"access_token", at},
+                {"id_token", idt},
+                {"refresh_token", rt},
+                {"token_type", tt},
+                {"expires_at", ea}
+            };
+            return oidc;
+        }
+        public async Task OnGet(string error  = null)
+        {
+            Error = error;
+            if (string.IsNullOrEmpty(Error))
+            {
+                OIDC = await HarvestOidcDataAsync();
+            }
+          
+        }
+    }
+}
