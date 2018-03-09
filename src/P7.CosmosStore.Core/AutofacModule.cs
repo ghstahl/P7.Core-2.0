063234096cc8bc6e8339b41359511c780a3f9154
ghstahl@gmail.com <ghstahl@gmail.com>
2018-03-09 10:22:59 -0800

    placeholder for a GraphQL interface to Cosmos

---
+++ src/P7.CosmosStore.Core/AutofacModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace P7.CosmosStore.Core
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+           
+        }
+    }
+}
